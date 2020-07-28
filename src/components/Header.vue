1203f875b33071b58923605d1643c8027f3e69c8
Nihir Shah <nihir10dec@gmail.com>
2020-07-28 21:58:06 +0530

    scores added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,6 @@
     <b-navbar-brand href="/">MyQuiz</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">Score</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
   </b-navbar>
 </template>
 
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,7 +1,7 @@
 <template>
-  <b-container>
+  <b-container fluid>
     <b-row>
-      <b-col sm="3">
+      <b-col sm="2">
         <div>
           <p>Question Navigation</p>
           <b-avatar
@@ -13,45 +13,49 @@
             variant="info"
             @click="toogleQuestion(i)"
             :class="selectedAnswer[i - 1] !== undefined ? 'answered' : ''"
-            >{{ i }}</b-avatar
-          >
+          >{{ i }}</b-avatar>
         </div>
       </b-col>
-      <b-col sm="9">
+      <b-col sm="7">
         <b-jumbotron bg-variant="info">
-          <template v-slot:lead
-            >Q{{ index + 1 }} {{ questions[index].question }}</template
-          >
+          <template v-slot:lead>Q{{ currentQuestion + 1 }} {{ questions[currentQuestion].question }}</template>
 
           <hr class="my-4" />
 
           <b-list-group>
             <b-list-group-item
               button
-              v-for="(answer, index) in answers"
+              v-for="(answer, index) in allanswers[currentQuestion]"
               :key="answer"
               @click.prevent="selectAnswer(index)"
               :class="ansclass(index)"
-              >{{ index + 1 }}.) {{ answer }}</b-list-group-item
-            >
+            >{{ index + 1 }}.) {{ answer }}</b-list-group-item>
           </b-list-group>
           <br />
-          <b-button
-            variant="success"
-            @click="prevquestion"
-            :disabled="index === 0"
-          >
-            <b-icon icon="arrow-left-circle-fill"></b-icon>&nbsp; Prev </b-button
-          >&nbsp; <b-button variant="primary">Submit</b-button>&nbsp;
+          <b-button variant="success" @click="prevquestion" :disabled="currentQuestion === 0">
+            <b-icon icon="arrow-left-circle-fill"></b-icon>&nbsp; Prev
+          </b-button>&nbsp;
+          <b-button variant="primary" @click="quizSUbmit" :disabled="quizSubmitted">Submit</b-button>&nbsp;
           <b-button
             variant="success"
             @click="nextquestion"
-            :disabled="index === questions.length - 1"
+            :disabled="currentQuestion === questions.length - 1"
           >
             Next&nbsp;
             <b-icon icon="arrow-right-circle-fill"></b-icon>
           </b-button>
         </b-jumbotron>
+      </b-col>
+      <b-col sm="3" v-if="quizSubmitted">
+        <b-card-group deck>
+          <b-card header="SCORES" header-tag="header">
+            <b-list-group-item variant="success">Score: {{score}} / {{questions.length}}</b-list-group-item>
+            <b-list-group-item variant="primary">Attempted: {{attempted}}</b-list-group-item>
+            <b-list-group-item
+              variant="warning"
+            >Accuracy: {{Number((score/attempted*100).toFixed(2))}}</b-list-group-item>
+          </b-card>
+        </b-card-group>
       </b-col>
     </b-row>
   </b-container>
@@ -62,8 +66,12 @@ export default {
   name: "QuestionBox",
   data() {
     return {
-      index: 0,
+      currentQuestion: 0,
       selectedAnswer: [],
+      allanswers: [],
+      quizSubmitted: false,
+      score: 0,
+      attempted: 0,
     };
   },
   props: {
@@ -71,32 +79,63 @@ export default {
   },
   methods: {
     prevquestion() {
-      this.index--;
+      this.currentQuestion--;
     },
     nextquestion() {
-      this.index++;
+      this.currentQuestion++;
     },
     selectAnswer(i) {
-      this.selectedAnswer[this.index] = i;
+      this.selectedAnswer[this.currentQuestion] = i;
     },
     toogleQuestion(i) {
-      this.index = i - 1;
+      this.currentQuestion = i - 1;
     },
     ansclass(i) {
-      if (this.selectedAnswer[this.index] === i) {
+      let correctindex = this.allanswers[this.currentQuestion].indexOf(
+        this.questions[this.currentQuestion].correct_answer
+      );
+      if (this.quizSubmitted && correctindex === i) {
+        return "correct";
+      }
+      if (
+        this.quizSubmitted &&
+        correctindex !== i &&
+        this.selectedAnswer[this.currentQuestion] === i
+      ) {
+        return "wrong";
+      }
+      if (
+        !this.quizSubmitted &&
+        this.selectedAnswer[this.currentQuestion] === i
+      ) {
         return "selected";
       }
     },
-  },
-  computed: {
-    answers() {
-      // console.log(this.questions);
-      let answers = [...this.questions[this.index].incorrect_answers];
-      answers.push(this.questions[this.index].correct_answer);
-      console.log(this.questions[this.index].correct_answer);
-      answers.sort(() => Math.random() - 0.5);
-      return answers;
+    quizSUbmit() {
+      this.quizSubmitted = true;
+      for (let j = 0; j < this.questions.length; j++) {
+        if (this.selectedAnswer[j] !== undefined) {
+          if (
+            this.allanswers[j][this.selectedAnswer[j]] ===
+            this.questions[j].correct_answer
+          ) {
+            this.score++;
+          }
+          this.attempted++;
+        }
+      }
     },
+  },
+  mounted() {
+    // console.log(this.questions);
+    for (let j = 0; j < this.questions.length; j++) {
+      let answers = [...this.questions[j].incorrect_answers];
+      answers.push(this.questions[j].correct_answer);
+      console.log(this.questions[j].correct_answer);
+      answers.sort(() => Math.random() - 0.5);
+      this.allanswers[j] = answers;
+    }
+    console.log(this.allanswers);
   },
 };
 </script>
@@ -107,5 +146,11 @@ export default {
 }
 .answered {
   background-color: deepskyblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.wrong {
+  background-color: indianred;
 }
 </style>
